eca1a516043e335bbfd07ff8e0b0356a6d45c4a6
ImeevOchir <ochir.imee@gmail.com>
2022-06-27 13:59:57 +0300

    Deploy to test with workflow Create files upload_output.sh .github/workflow/deploy-branch.yml and .github/workflow/destroy_deployment.yml
    Resloves #7

---
+++ upload_output.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+BRANCH=$BRANCH_NAME
+ENDPOINT_URL=$S3_ENDPOINT_URL
+DOC_DEST="$S3_UPLOAD_PATH/internals/${BRANCH_NAME}"
+
+aws s3 cp output/json "$DOC_DEST"/json --endpoint-url="$ENDPOINT_URL" --recursive --include "*" --exclude "*.jpg" --exclude "*.png" --exclude "*.svg"
+
+set -xe
+curl --fail --show-error \
+    --data '{"update_key":"'"$INTERNALS_UPDATE_KEY"'"}' \
+    --header "Content-Type: application/json" \
+    --request POST "${INTERNALS_UPDATE_URL}""${BRANCH}"/
