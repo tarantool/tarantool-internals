ca3f1330c070586e08198783cbb794453dbd1b2f
Nick Volynkin <nick.volynkin@gmail.com>
2022-08-17 20:36:27 +0700

    Fail-fast workflows and reports to VK Teams
    Use strict settings for fast-failing on errors in
    bash scripts.
    
    Report failed workflows to VK Teams.
    
    Rename a few variables for consistency with other doc projects.

--- upload_output.sh
+++ upload_output.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-set -xe
-DOC_DEST="$S3_UPLOAD_PATH/internals/${BRANCH_NAME}"
-# version in doc project and scripts
-DOC_VERSION=latest
-CHECK_KEY="doc-builds/internals/${BRANCH_NAME}/json/_build_en/json/toctree.fjson"
-UPDATE_KEY=$INTERNALS_UPDATE_KEY
-UPDATE_URL=$INTERNALS_UPDATE_URL
+set -xe -o pipefail -o nounset
 
+DOC_DEST="$S3_UPLOAD_PATH/internals/${DEPLOYMENT_NAME}"
+# version in doc project and scripts is always 'latest'
+# for both prod and test server, because internals
+# is a single-version project.
+DOC_VERSION=latest
+# directory in the S3 bucket: latest or test.
+CHECK_KEY="doc-builds/internals/${DEPLOYMENT_NAME}/json/_build_en/json/toctree.fjson"
+
+not_exist=false
 aws s3api head-object --bucket ${S3_BUCKET} --key ${CHECK_KEY} --endpoint-url="${S3_ENDPOINT_URL}" || not_exist=true
 if [ $not_exist ]; then
   echo "toctree.json does not exist"
@@ -17,8 +20,3 @@ else
 fi
 
 aws s3 cp build/json "${DOC_DEST}"/json/_build_en/json --endpoint-url="${S3_ENDPOINT_URL}" --recursive --include "*" --exclude "*.jpg" --exclude "*.png" --exclude "*.svg"
-
-curl --fail --show-error \
-    --data '{"update_key":"'"${UPDATE_KEY}"'"}' \
-    --header "Content-Type: application/json" \
-    --request POST "${UPDATE_URL}""${DOC_VERSION}"/
